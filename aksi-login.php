e5490e5b484ee7bdb226b213c5a60c846f466adc
maulayyacyber <ridaulmaulayya@gmail.com>
2018-08-10 14:06:47 +0700

    nambah komentar di koding aksi login

--- aksi-login.php
+++ aksi-login.php
@@ -15,8 +15,10 @@ $password = MD5($_POST['password']);
 //query data admin
 $query_admin = mysqli_query($connection, "SELECT * FROM tbl_admin WHERE username = '$username' AND password = '$password'");
 
+//cek jumlah data admin
 $rs_admin = mysqli_num_rows($query_admin);
 
+//kondisi jika data diatas 0
 if ($rs_admin  > 0){
 
 	$_SESSION['username'] 	= $username;
@@ -30,8 +32,10 @@ if ($rs_admin  > 0){
 	//query data dosen
 	$query_dosen = mysqli_query($connection, "SELECT * FROM tbl_dosen WHERE nidn = '$username' AND password = '$password'");
 
+	//cek jumlah data dosen
 	$rs_dosen = mysqli_num_rows($query_dosen);
 
+	//kondisi jika data diatas 0
 	if ($rs_dosen  > 0){
 
 		$_SESSION['username'] 	= $username;
@@ -45,8 +49,10 @@ if ($rs_admin  > 0){
 		//query data mahasiswa
 		$query_mhs = mysqli_query($connection, "SELECT * FROM tbl_mahasiswa WHERE nim = '$username' AND password = '$password'");
 
+		//cek jumlah data mahasiswa
 		$rs_mhs = mysqli_num_rows($query_mhs);
 
+		//kondisi jika data diatas 0
 		if ($rs_mhs  > 0){
 
 			$_SESSION['username'] 	= $username;
