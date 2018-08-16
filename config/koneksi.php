65445489f3675efeef8fbef043115a2a90cffdf5
maulayyacyber <ridaulmaulayya@gmail.com>
2018-08-16 17:15:54 +0700

    hahahaha

--- config/koneksi.php
+++ config/koneksi.php
@@ -4,11 +4,7 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-<<<<<<< HEAD
 $database = "website-penelitian";
-=======
-$database = "website_penelitian";
->>>>>>> d0fb0a0bf432b3b2ad7ac954235d44b9368e03c8
 //create variable connectin
 $connection = mysqli_connect($hostname, $username, $password, $database);
 //checking connection
