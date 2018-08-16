3ae6bc4a244df9a39502a23f062fbfee53353ac0
miyah0910 <khicmiyah@gmail.com>
2018-08-16 17:24:16 +0700

    ganti mahasiswa

---
+++ admin/simpan-mahasiswa.php
@@ -0,0 +1,23 @@
+<?php
+
+//manggil koneksi database
+include('../config/koneksi.php');
+
+//deklasri variabel dari FROM
+$nama 		= $_POST['nama'];
+$nim 		= $_POST['nim'];
+$password 	= MD5($_POST['password']);
+
+//query
+$query = "INSERT INTO tbl_mahasiswa (nama_mahasiswa, nim, password)
+VALUES ('$nama', '$nim', '$password')";
+
+if ($connection->query($query) === TRUE) {
+
+	//echo "data berhasil disimpan";
+	header("location:mahasiswa.php");
+
+}else{
+
+	echo "data gagal disimpan";
+}
