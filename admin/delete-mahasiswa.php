5441dcf353e52be0c23cbc6f05d5bfe9cde74248
maulayyacyber <ridaulmaulayya@gmail.com>
2018-08-17 17:21:44 +0700

    fix delete mahasiswa

--- admin/delete-mahasiswa.php
+++ admin/delete-mahasiswa.php
@@ -12,17 +12,12 @@ $id = $_GET['id'];
 $query = "DELETE FROM tbl_mahasiswa WHERE nim = '$id'";
 $result = mysqli_query($connection, $query);
 if ($connection->query($query) === TRUE) {
-<<<<<<< HEAD
+
 	 header("location:mahasiswa.php"); 
-=======
-    echo "Delete record  successfully";
-    echo "<br>";
-    echo "<button><a href='index.php'>Kembali</a></button>";
->>>>>>> 0cbfafd87639f85561c06351d89f79377edb5723
+
 } else {
     echo "Error: " . $query . "<br>" . $connection->error;
     echo "<br>";
     echo "<button><a href='tambah-dosen.php'>Tambah Data Dosen</a></button>";
 }
  ?>
-© 2018 GitHub, Inc.
