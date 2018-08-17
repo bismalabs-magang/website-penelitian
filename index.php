38db7216d2ededf360206812bab4fc176c0bcb80
Ghonirwan <ghonir330@gmail.com>
2018-08-17 01:36:21 -0700

    menambah file index.php

--- index.php
+++ index.php
@@ -1,70 +1,108 @@
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title></title>
-     <!-- Favicon-->
+    <!-- Favicon-->
     <link rel="icon" href="../images/logo.png" type="image/x-icon">
-
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
-
     <!-- Bootstrap Core Css -->
     <link href="../assets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
-
     <!-- Waves Effect Css -->
     <link href="../assets/plugins/node-waves/waves.css" rel="stylesheet" />
-
     <!-- Animation Css -->
     <link href="../assets/plugins/animate-css/animate.css" rel="stylesheet" />
-
     <!-- JQuery DataTable Css -->
     <link href="../assets/plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
-
     <!-- Morris Chart Css-->
     <link href="../assets/plugins/morrisjs/morris.css" rel="stylesheet" />
-
     <!-- Custom Css -->
     <link href="../assets/css/style.css" rel="stylesheet">
-
     <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
     <link href="../assets/css/themes/all-themes.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
-</head>
-<body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
+  </head>
+  <body>
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Semua Judul Skripsi</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Semua Judul Penelitian</a>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Lab Penelitian</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+      </div>
 
-</body>
-</html>
+      <br>
+      <!-- start content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row clearfix">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="header">
+                  
+                  <!-- Main jumbotron for a primary marketing message or call to action -->
+                  <div class="jumbotron" style="background-image: url('<?php echo $config['base_url'] ?>assets/img/bg.png'); position:relative;background-color:#1abc9c">
+                    <div class="container" style="color:#3cb371">
+                      <h2 style="font-family: 'Lato'; font-weight:bold;font-size:35px">Universitas KH. A. Wahab Hasbullah</h2>
+                      <p style="font-family: 'Lato'; font-weight:bold;font-size:20px">Untuk mengajukan sebuah judul skripsi mahasiswa diharuskan untuk masuk panel system dan mengajukan minimal 2 buah judul, mahasiswa juga boleh mengambil tema dari judul penelitian dosen yang sudah disetujui oleh dosen tersebut</p>
+                      <div class="row">
+                        <p style="font-family: 'ubuntu'; font-weight:normal;font-size:20px"><a class="btn btn-default btn-lg" href="mahasiswa/" role="button"><i class="fa fa-lock"></i> Masuk Mahasiswa <i class="fa fa-long-arrow-right"></i></a></p>
+                        <a class="btn btn-default btn-lg" href="mahasiswa/" role="button"><i class="fa fa-lock"></i> Masuk Dosen <i class="fa fa-long-arrow-right"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="header-dropdown m-r--5">
+                  </div>
+                </div>
+                <div class="body">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- end content -->
+      
+      <br>
+      <!-- Example row of columns -->
+      <div class="col-auto mr-auto">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="panel-heading" style="color: #fff;background-color: #3cb371;border-color:#3cb371;font-family: 'ubuntu'; font-weight:bold;font-size:35px">
+              <h5 class="panel-title"><i class="fa fa-bell"></i> INFORMASI </h5>
+            </div>
+            <div class="panel-body">
+              <p><br />
+              &nbsp;</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr>
+
+      <!-- footer -->
+      <div class="col-auto mr-auto">
+        <footer>
+          <p style="font-family: 'ubuntu'; font-weight:normal;font-size:15px">Copyright &copy; 2018 Universitas KH. A. Wahab Hasbullah, All Rights Reserved.</p>
+        </footer>
+      </div>
+    </body>
+  </html>
