6e655c12b667f4fa216c1d8f1f38489de6c50621
unknown <mirasugiarti14@gmail.com>
2018-08-17 16:56:36 +0700

    data-delete-fakultas

---
+++ admin/delete-fakultas.php
@@ -0,0 +1,19 @@
+<?php
+
+//include koneksi ke database
+include_once('../config/koneksi.php');
+//get nidn
+$id = $_GET['id'];
+$query = "DELETE FROM tbl_fakultas WHERE id_fakultas = '$id'";
+$result = mysqli_query($connection, $query);
+if ($connection->query($query) === TRUE) {
+ 
+    //echo "data berhasil disimpan";
+    header("location:fakultas.php");
+
+} else {
+    echo "Error: " . $query . "<br>" . $connection->error;
+    echo "<br>";
+
+}
+ ?>
