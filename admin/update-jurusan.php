f0121569b792ba72d94940d71659d8e03e4a185e
unknown <mirasugiarti14@gmail.com>
2018-08-17 17:27:28 +0700

    edit-data-delete-jurusan

--- admin/update-jurusan.php
+++ admin/update-jurusan.php
@@ -10,10 +10,10 @@ $query = "UPDATE tbl_jurusan SET nama_jurusan = '$nama'  WHERE id_jurusan = '$id
 
 
 if ($connection->query($query) === TRUE) {
-    header("location:fjurusan.php");
+    header("location:jurusan.php");
 } else {
     echo "Error: " . $query . "<br>" . $connection->error;
     echo "<br>";
-    echo "<button><a href='tambah-fakultas.php'>Tambah Data Jurusan</a></button>";
+    echo "<button><a href='tambah-jurusan.php'>Tambah Data Jurusan</a></button>";
 }
  ?>
