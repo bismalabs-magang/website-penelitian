eede25fe3893a9a52e7dc160331fe92df189955a
miyah0910 <khicmiyah@gmail.com>
2018-08-17 17:48:01 +0700

    menu mahasiswa

---
+++ mahasiswa/part/footer.php
@@ -0,0 +1,49 @@
+<!-- Jquery Core Js -->
+<script src="../assets/plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="../assets/plugins/bootstrap/js/bootstrap.js"></script>
+
+<!-- Select Plugin Js -->
+<script src="../assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+<!-- Slimscroll Plugin Js -->
+<script src="../assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+<!-- Waves Effect Plugin Js -->
+<script src="../assets/plugins/node-waves/waves.js"></script>
+
+<!-- Jquery CountTo Plugin Js -->
+<script src="../assets/plugins/jquery-countto/jquery.countTo.js"></script>
+
+<!-- Morris Plugin Js -->
+<script src="../assets/plugins/raphael/raphael.min.js"></script>
+<script src="../assets/plugins/morrisjs/morris.js"></script>
+
+<!-- ChartJs -->
+<script src="../assets/plugins/chartjs/Chart.bundle.js"></script>
+
+<!-- Sparkline Chart Plugin Js -->
+<script src="../assets/plugins/jquery-sparkline/jquery.sparkline.js"></script>
+
+<!-- Jquery DataTable Plugin Js -->
+<script src="../assets/plugins/jquery-datatable/jquery.dataTables.js"></script>
+<script src="../assets/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+<script src="../assets/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+<script src="../assets/plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+<script src="../assets/plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+<script src="../assets/plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+<script src="../assets/plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+<script src="../assets/plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+<script src="../assets/plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+<!-- Custom Js -->
+<script src="../assets/js/admin.js"></script>
+<!--<script src="../assets/js/pages/index.js"></script>-->
+<script src="../assets/js/pages/tables/jquery-datatable.js"></script>
+
+<!-- Demo Js -->
+<script src="../assets/js/demo.js"></script>
+</body>
+
+</html>
