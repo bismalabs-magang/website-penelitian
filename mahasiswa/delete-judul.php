29665a06eb32eb981d7de1ad9f3c644d6fb6f31b
miyah0910 <khicmiyah@gmail.com>
2018-08-19 17:19:09 +0700

    nambah ajukan judul

---
+++ mahasiswa/delete-judul.php
@@ -0,0 +1,16 @@
+<?php
+
+//include koneksi ke database
+include_once('../config/koneksi.php');
+//get nim
+$id = $_GET['id'];
+$query = "DELETE FROM tbl_skripsi WHERE id_skripsi = '$id'";
+$result = mysqli_query($connection, $query);
+if ($connection->query($query) === TRUE) {
+	 header("location:ajukan-judul.php");
+} else {
+	echo "Error: " . $query . "<br>" . $connection->error;
+	echo "<br>";
+	echo "<button><a href='tambah-judul.php'>Tambah Data Ajuan Judul</a></button>";
+}
+ ?>
