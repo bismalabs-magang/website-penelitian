fa216e26464270cb117fea6088ba8902e4843017
maulayyacyber <ridaulmaulayya@gmail.com>
2018-08-20 17:08:27 +0700

    add delete file from server

--- dosen/delete-penelitian.php
+++ dosen/delete-penelitian.php
@@ -2,9 +2,22 @@
 
 //include koneksi ke database
 include_once('../config/koneksi.php');
+
 //get nidn
 $id = $_GET['id'];
+
+//select data database
+$query_data = "SELECT file_penelitian FROM tbl_penelitian WHERE id_penelitian = '$id'";
+
+$result_data = mysqli_query($connection, $query_data);
+$row_data = mysqli_fetch_array($result_data);
+
+//delete file berdasarkan nama file
+$path = "../file/".$row_data['file_penelitian'];
+unlink($path); 
+
 $query = "DELETE FROM tbl_penelitian WHERE id_penelitian = '$id'";
+
 $result = mysqli_query($connection, $query);
 if ($connection->query($query) === TRUE) {
 	 header("location:penelitian.php");
